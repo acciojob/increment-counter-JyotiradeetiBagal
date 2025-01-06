d20d8c78fdac9ba6f9619ac228453adf78a69661
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-06 16:51:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn=document.getElementById("incrementBtn");
+let count=document.getElementById("counter");
+let i=0;
+btn.addEventListener("click",()=>{
+	alert(i);
+	count.innerHTML = ++i;
+
+});
